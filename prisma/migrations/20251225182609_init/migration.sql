91c03b3204a812ac4ebd21f872fd30ddea41c110
Vivek Kumar <vh118854@gmail.com>
2025-12-26 00:03:31 +0530

    prisma logic start

---
+++ prisma/migrations/20251225182609_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "menu_items" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "menu_items_pkey" PRIMARY KEY ("id")
+);
